67f0a751aeb57c780e19f80fe125dfd32a3c0e66
Amir Maghami <Maghami.A1987@gmail.com>
2025-01-18 17:49:46 +0330

    fix: config

---
+++ server/src/config/pgConfig.ts
@@ -0,0 +1,9 @@
+import { PostgresConnectionOptions } from 'typeorm/driver/postgres/PostgresConnectionOptions';
+
+export const pgConfig: PostgresConnectionOptions = {
+  type: 'postgres',
+  url: 'postgresql://neondb_owner:A8TEdDWzvB6b@ep-steep-star-a8mx1f7u.eastus2.azure.neon.tech/neondb?sslmode=require',
+  port: 3306,
+  entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+  synchronize: true, // only "true" develop mod
+};
