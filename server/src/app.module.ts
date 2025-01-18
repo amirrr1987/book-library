44653c4ea9cdda745f56ff8517899052fb4c7538
Amir Maghami <Maghami.A1987@gmail.com>
2025-01-18 16:05:02 +0330

    feat create genres res

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BooksModule } from './books/books.module';
 import { AuthorsModule } from './authors/authors.module';
+import { GenresModule } from './genres/genres.module';
 
 @Module({
-  imports: [BooksModule, AuthorsModule],
+  imports: [BooksModule, AuthorsModule, GenresModule],
   controllers: [AppController],
   providers: [AppService],
 })
